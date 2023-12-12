ebe02507d67f3ed7f22477a2710bd1710d743b05
mynternet <mynternet@gmail.com>
2023-12-12 11:32:46 -0500

    proxies update

--- src/App.js
+++ src/App.js
@@ -14,19 +14,20 @@ import SearchItem from "./pages/SearchItem";
 import Navbar from "./components/Navbar";
 import ItemDetail from "./pages/ItemDetail";
 import SwapItems from "./pages/SwapItems";
-import AcceptSwapItems from "./pages/AcceptSwapItems"
+import AcceptSwapItems from "./pages/AcceptSwapItems";
 import SwapHistory from "./pages/SwapHistory";
 import About from "./pages/About"; // Import About page
 import Contact from "./pages/Contact"; // Import Contact page
 import Footer from "./pages/Footer"; // Import Footer component
 
+// Setup the link to the GraphQL server
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "https://backend-dot-swapcomm.ue.r.appspot.com/graphql",
 });
 
+// Setup the authentication link
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
-
   return {
     headers: {
       ...headers,
@@ -35,6 +36,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+// Create the Apollo Client instance
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
@@ -53,8 +55,8 @@ function App() {
             <Route path="/swap/:id/:ownerUsername" element={<SwapItems />} />
             <Route path="/accept-swap-items/:itemToChange" element={<AcceptSwapItems />} />
             <Route path="/swap-history" element={<SwapHistory />} />
-            <Route path="/about" element={<About />} />  {/* About page route */}
-            <Route path="/contact" element={<Contact />} />  {/* Contact page route */}
+            <Route path="/about" element={<About />} /> {/* About page route */}
+            <Route path="/contact" element={<Contact />} /> {/* Contact page route */}
           </Routes>
           <Footer /> {/* Footer component */}
         </>
